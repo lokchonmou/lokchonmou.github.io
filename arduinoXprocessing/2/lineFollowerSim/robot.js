fa2759b3edaad050d21f4f4f790b700b178c0bbd
lokchonmou <lokchonmou@users.noreply.github.com>
2022-08-14 22:52:59 +0800

    version 2.0
    Huge update
    1. save and load feature
    2. GUI is created when the car is created, and show only for the current car
    3. circle out the current car and the winner car
    4. delete the car when it is out of field

--- arduinoXprocessing/2/lineFollowerSim/robot.js
+++ arduinoXprocessing/2/lineFollowerSim/robot.js
@@ -1,17 +1,15 @@
 class Robot{
 
-    params = {
-        robotWidth : 30,        //default 30
-        sensorNo : 7,           //default 7
-        sensorSize : 4,         //default 4
-        sensor_distance : 40,   //default 40
-        sensor_width : 5,       //default 5
-        maxAccel : .5,          //default .5  代表有跣軚, 0是完全跣軚不會行走, max是maxVel
-        maxVel : 7.,            //default 7.
-        Kp : 2.1,
-        Kd : 1.2,                 //Core PID
-    };
+    robotWidth = 30;        //default 30
+    sensorNo = 7;           //default 7
+    sensor_distance = 40;   //default 40
+    sensor_width = 5;       //default 5
+    maxAccel = .5;          //default .5  代表有跣軚, 0是完全跣軚不會行走, max是maxVel
+    maxVel = 7.;            //default 7.
+    Kp = 2.1;
+    Kd = 1.2;                 //Core PID
 
+    sensorSize = 4;
     id = 0;                 //the ID of the current car
     state = [];             //the state of the sensor array, true is on black line, false is on white bg
     vr=0.; vl=0.;           //velocity of the left and right wheel
@@ -25,12 +23,14 @@ class Robot{
     enterTimerZone;
     score=0;
 
+    GUI;
+
     constructor(_id, _x, _y, _theta) {
         this.id = _id;
         this.x = _x;
         this.y = _y;
         this.theta = _theta;
-        for(var i = 0; i < this.params.sensorNo; i++)
+        for(var i = 0; i < this.sensorNo; i++)
         this.state[i] = false;
         ellipseMode(CENTER);
         this.enterTimerZone = true;
@@ -39,6 +39,7 @@ class Robot{
         this.timer = frameCount;
         this.last_timer = this.timer;
         this.score = 0;
+
     }
 
     updateSensor(){
@@ -53,21 +54,21 @@ class Robot{
 
         if (this.x < 0 || this.x > 600 || this.y < 0 || this.y > 600 ) this.isDead = true;
 
-        for (var i=0; i<this.params.sensorNo; i++) {
+        for (var i=0; i<this.sensorNo; i++) {
             _theta = radians(this.theta);
-            thisSensorX = this.params.sensor_distance*sin(this.params.sensor_width*(i-floor(this.params.sensorNo/2))/this.params.sensor_distance);
-            thisSensorY = this.params.sensor_distance*cos(this.params.sensor_width*(i-floor(this.params.sensorNo/2))/this.params.sensor_distance);
+            thisSensorX = this.sensor_distance*sin(this.sensor_width*(i-floor(this.sensorNo/2))/this.sensor_distance);
+            thisSensorY = this.sensor_distance*cos(this.sensor_width*(i-floor(this.sensorNo/2))/this.sensor_distance);
             sensor_position_x = this.x + thisSensorX*cos(_theta) + thisSensorY*sin(_theta);
             sensor_position_y = this.y + thisSensorX*sin(_theta) - thisSensorY*cos(_theta);
 
             var blackValue=0;
-            for (var j=0; j < this.params.sensorSize; j++){
-                for (var k=0; k<this.params.sensorSize; k++){
+            for (var j=0; j < this.sensorSize; j++){
+                for (var k=0; k<this.sensorSize; k++){
                     var c = get(int(sensor_position_x)+j-floor(j/2), +int(sensor_position_y)+k-floor(k/2));
                     blackValue += (c[0]+c[1]+c[2])/3.;
                 }
             }
-            blackValue = 255. - blackValue/(this.params.sensorSize*this.params.sensorSize);
+            blackValue = 255. - blackValue/(this.sensorSize*this.sensorSize);
 
             if (blackValue > 127) this.state[i] = true;
             else this.state[i] = false;
@@ -90,40 +91,40 @@ class Robot{
     }
 
     setPD(_Kp, _Kd){
-        this.params.Kp = _Kp;
-        this.params.Kd = _Kd;
+        this.Kp = _Kp;
+        this.Kd = _Kd;
     }
 
     PID(){
         // PID controller////////////////////////////////
-        this.error = this.drift/1000. - (this.params.sensorNo - 1)/2.;
-        this.output = this.params.Kp * this.error+ this.params.Kd *(this.error - this.last_error);
+        this.error = this.drift/1000. - (this.sensorNo - 1)/2.;
+        this.output = this.Kp * this.error+ this.Kd *(this.error - this.last_error);
 
-        // output = params.Kp.getValueF() * input;
-        this.output = constrain(this.output, -2*this.params.maxVel, 2*this.params.maxVel);
+        // output = Kp.getValueF() * input;
+        this.output = constrain(this.output, -2*this.maxVel, 2*this.maxVel);
         this.last_error = this.error;
 
         if (this.output >0) {
-            this.desiredVl = this.params.maxVel;
-            this.desiredVr = this.params.maxVel - this.output;
+            this.desiredVl = this.maxVel;
+            this.desiredVr = this.maxVel - this.output;
         } else {
-            this.desiredVl = this.params.maxVel + this.output;
-            this.desiredVr = this.params.maxVel;
+            this.desiredVl = this.maxVel + this.output;
+            this.desiredVr = this.maxVel;
         }
     }
 
     drive(){
-        this.desiredVl = constrain(this.desiredVl, -this.params.maxVel, this.params.maxVel);
-        this.desiredVr = constrain(this.desiredVr, -this.params.maxVel, this.params.maxVel);
+        this.desiredVl = constrain(this.desiredVl, -this.maxVel, this.maxVel);
+        this.desiredVr = constrain(this.desiredVr, -this.maxVel, this.maxVel);
 
-        this.vl = (this.desiredVl - this.vl > 0)? this.vl+ this.params.maxAccel: this.vl-this.params.maxAccel;
-        this.vr = (this.desiredVr - this.vr > 0)? this.vr+ this.params.maxAccel: this.vr-this.params.maxAccel;
+        this.vl = (this.desiredVl - this.vl > 0)? this.vl+ this.maxAccel: this.vl-this.maxAccel;
+        this.vr = (this.desiredVr - this.vr > 0)? this.vr+ this.maxAccel: this.vr-this.maxAccel;
 
-        this.vl = constrain(this.vl, -this.params.maxVel, this.params.maxVel);
-        this.vr = constrain(this.vr, -this.params.maxVel, this.params.maxVel);
+        this.vl = constrain(this.vl, -this.maxVel, this.maxVel);
+        this.vr = constrain(this.vr, -this.maxVel, this.maxVel);
 
-        // var R = -this.params.robotWidth*(this.vl+this.vr)/(2*(this.vr-this.vl));
-        var dtheta=-(this.vr-this.vl)/this.params.robotWidth;
+        // var R = -this.robotWidth*(this.vl+this.vr)/(2*(this.vr-this.vl));
+        var dtheta=-(this.vr-this.vl)/this.robotWidth;
         var dx= (this.vr+this.vl)/2 * sin(radians(this.theta));
         var dy=-(this.vr+this.vl)/2 * cos(radians(this.theta));
         // this.x = -R*sin(radians(this.theta)) + R*sin(dtheta+radians(this.theta)) + this.x;
@@ -150,16 +151,16 @@ class Robot{
             //draw the body of car
             fill('#325AA8');
             rectMode(CORNER);
-            rect(-this.params.robotWidth/2., -20.0, this.params.robotWidth, 30.);
+            rect(-this.robotWidth/2., -20.0, this.robotWidth, 30.);
             //draw the two wheels
             fill('#f2511f');
             rectMode(CENTER);
-            rect(-this.params.robotWidth/2-this.wheelWidth/2, 0,this.wheelWidth, 20);
-            rect(this.params.robotWidth/2+this.wheelWidth/2, 0,this.wheelWidth, 20);
+            rect(-this.robotWidth/2-this.wheelWidth/2, 0,this.wheelWidth, 20);
+            rect(this.robotWidth/2+this.wheelWidth/2, 0,this.wheelWidth, 20);
             //draw the center line of car
             stroke('#FF00FF');
             strokeWeight(.5);
-            line(-this.params.robotWidth/2.-this.wheelWidth-10, 0, this.params.robotWidth/2+this.wheelWidth/2+10, 0);
+            line(-this.robotWidth/2.-this.wheelWidth-10, 0, this.robotWidth/2+this.wheelWidth/2+10, 0);
             line(0, -30, 0, 15);
             //print the id of car on the top
             fill(255); noStroke();
@@ -172,22 +173,22 @@ class Robot{
         noStroke();
         text(this.score, this.x+25, this.y-50);
 
-        for (var i=0; i<this.params.sensorNo; i++) {
+        for (var i=0; i<this.sensorNo; i++) {
             _theta = radians(this.theta);
-            thisSensorX = this.params.sensor_distance*sin(this.params.sensor_width*(i-floor(this.params.sensorNo/2))/this.params.sensor_distance);
-            thisSensorY = this.params.sensor_distance*cos(this.params.sensor_width*(i-floor(this.params.sensorNo/2))/this.params.sensor_distance);
+            thisSensorX = this.sensor_distance*sin(this.sensor_width*(i-floor(this.sensorNo/2))/this.sensor_distance);
+            thisSensorY = this.sensor_distance*cos(this.sensor_width*(i-floor(this.sensorNo/2))/this.sensor_distance);
             sensor_position_x = this.x + thisSensorX*cos(_theta) + thisSensorY*sin(_theta);
             sensor_position_y = this.y + thisSensorX*sin(_theta) - thisSensorY*cos(_theta);
             // draw the sensor array of car
             noFill();
             strokeWeight(.5);
             stroke(0);
-            ellipse(sensor_position_x, sensor_position_y, this.params.sensorSize, this.params.sensorSize);
+            ellipse(sensor_position_x, sensor_position_y, this.sensorSize, this.sensorSize);
             // draw the result of sensor array///////////////////////////////////
-            var thisSensorResultX = 8.*(i-floor(this.params.sensorNo/2));
+            var thisSensorResultX = 8.*(i-floor(this.sensorNo/2));
             var thisSensorResultY = 50.;
             fill(this.state[i]==true?'#FF0000':'FFFFFF');
-            ellipse(this.x+thisSensorResultX, this.y-thisSensorResultY, this.params.sensorSize, this.params.sensorSize);
+            ellipse(this.x+thisSensorResultX, this.y-thisSensorResultY, this.sensorSize, this.sensorSize);
         }
 
 
@@ -214,5 +215,4 @@ class Robot{
             this.oneLoop();
         }
     }
-
 }
